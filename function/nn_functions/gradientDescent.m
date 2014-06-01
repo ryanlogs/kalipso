7d9c52f6ac7de96ed5130658de8afa8948757f38
ryanlogs <ryanlogs@gmail.com>
2014-06-01 19:48:25 +0530

    display after 1000 iterations

--- function/nn_functions/gradientDescent.m
+++ function/nn_functions/gradientDescent.m
@@ -3,6 +3,8 @@ function [cost nn_params] = gradientDescent(initial_nn_params,network, X, y, dig
 
 	[cost gradient] = nnCostFunction(initial_nn_params, network, X, y, digit, lambda);
 	nn_params = initial_nn_params - gradient;
-	fprintf('\nIteration %d | Cost %f',iter,cost);
+	if(mod(iter,1000)==0)
+		fprintf('\nIteration %d | Cost %f',iter,cost);
+	end	
 
 end	
