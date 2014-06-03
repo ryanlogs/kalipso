ec2c174737a6fff97f1f31647ee690f28dbbd63e
ryanlogs <ryanlogs@gmail.com>
2014-06-03 20:07:59 +0530

    checked error

--- digit_rec.m
+++ digit_rec.m
@@ -48,10 +48,10 @@ function [] = digit_rec(digit)
 		
 		%test it against CV
 		pred = predict(Theta,CV_X);
-		cv_acc =  mean(double(pred == CV_Y(:,2))) * 100;
+		cv_acc =  mean(double(pred == CV_Y)) * 100;
 		
 		pred = predict(Theta,Train_X);
-		train_acc = mean(double(pred == Train_Y(:,2))) * 100;
+		train_acc = mean(double(pred == Train_Y)) * 100;
 		x = [ x ; i ];
 		train = [ train ; train_acc];
 		cv = [ cv ; cv_acc];
