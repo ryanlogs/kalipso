8ccaa755e1c4f7b9fdd992c23dfe0d91d5a3fc0e
ryanlogs <ryanlogs@gmail.com>
2014-06-04 23:13:52 +0530

    officially confirmed nn_cost function was the cause
    added old nn cost function, renamed new one to kaupt

---
+++ alternate_train.m
@@ -0,0 +1,46 @@
+
+
+addpath('function\nn_functions');
+addpath('function\util');
+
+%load data
+
+%this loads Train_X, Train_y
+load('data\general\train.mat');
+%this loads CV_X, CV_y
+load('data\general\cv.mat');
+%this loads Test_X, Test_y
+load('data\general\test.mat');
+
+
+network=[size(Test_X,2); 50; 50; 10];
+num_layers = size(network,1);
+lambda = [ 1.2; 0.3; 0.3 ];
+iter = 100;
+
+%setting initial_nn_params
+initial_nn_params = [];
+for i = 1:num_layers-1
+	parm = randInitializeWeights(network(i),network(i+1));
+	initial_nn_params = [ initial_nn_params ; parm(:) ];
+end
+
+options = optimset('MaxIter', iter);
+
+%training NN, the digit value 0 is just a dummy value, not used inside
+costFunction = @(p) nnCostFunction(p, network, Train_X, Train_y, lambda);
+[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);	
+	
+%unrolling theta	
+Theta = cell(num_layers-1,1);
+read = 0;
+for i = 1:num_layers - 1
+	Theta{i} = reshape(nn_params(read + 1: read + network(i+1) * (network(i) + 1)), ...
+					network(i+1), network(i)+1);
+						
+	read = 	network(i+1) * (network(i) + 1);
+end
+
+pred = predict(Theta,Train_X);
+train_acc = mean(double(pred == Train_y)) * 100;		
+fprintf('\nTraining Accuracy: %f |\tlambda: %f\n', train_acc, i);	
--- function/nn_functions/nnCostFunction.m
+++ function/nn_functions/nnCostFunction.m
@@ -1,93 +1,93 @@
 function [J grad] = nnCostFunction(nn_params, ...
                                    network, ...
-                                   X, y, digit, lambda)
+                                   X, y, lambda)
 
 	
-	addpath('function\nn_functions');
 	m = size(X,1);	
-	
 	num_layers = length(network);
-	num_lables = network(num_layers);
-	Theta = cell(num_layers-1,1);
-	read = 0;
-
-	for i = 1:num_layers - 1
-		Theta{i} = reshape(nn_params(read + 1: read + network(i+1) * (network(i) + 1)), ...
-						network(i+1), network(i)+1);
-						
-		read = 	network(i+1) * (network(i) + 1);
-	end		
+	num_lables = network(4);
+	
+	input_layer_size = network(1);
+	hidden_layer1_size = network(2);
+	hidden_layer2_size = network(3);
+	output_layer_size = network(4);
+	
+	Theta1 = reshape(nn_params(1:hidden_layer1_size * (input_layer_size+1)), ...
+						hidden_layer1_size, input_layer_size+1);
+	read = 	hidden_layer1_size * (input_layer_size+1);
+	
+	Theta2 = reshape(nn_params(read + 1: read + hidden_layer2_size * (hidden_layer1_size+1)), ...
+						hidden_layer2_size, hidden_layer1_size+1);
+	read = read + hidden_layer2_size * (hidden_layer1_size+1);
+	
+	Theta3 = reshape(nn_params(read + 1 : end ), ...
+						output_layer_size, hidden_layer2_size+1);							
+	
 	
 % You need to return the following variables correctly 
 	J = 0;
-		
-% computing cost
-	A = cell(num_layers,1);
-	Z = cell(num_layers,1);
-	for i=1:num_layers
-		if(i==1)
-			A{i} = [ones(m,1) , X];
-		else
-			Z{i} = A{i-1} * (Theta{i-1})';
-			A{i} = sigmoid(Z{i});
-			if(i~=num_layers)
-				A{i} = [ ones(m,1) , A{i} ];
-			end		
-		end	
-	end
+	Theta1_grad = zeros(size(Theta1));
+	Theta2_grad = zeros(size(Theta2));	
+	Theta3_grad = zeros(size(Theta3));	
 	
-	%setting output vector	
+	
+	% computing cost
+	a1 = [ones(m,1), X];
+
+	z2 = (a1 * Theta1');
+	a2 = sigmoid(z2);	
+	a2 = [ones(m,1), a2];
+
+	z3 = (a2 * Theta2');
+	a3 = sigmoid(z3);	
+	a3 = [ones(m,1), a3];
+
+	z4 = (a3 * Theta3');
+	a4 = sigmoid(z4);
+
 	Y = zeros(m, num_lables);
 	for i = 1:m,
 		Y(i,y(i)+1) = 1;
 	end;
-
-	p1 = Y .* log(A{num_layers});
-	p2 = (1 - Y) .* log(1 - A{num_layers});
+	
+	p1 = Y .* log(a4);
+	p2 = (1 - Y) .* log(1 - a4);
 	
 	J = sum(p1 + p2) ;
 	J = sum(J) / (-1 * m);
-
-	reg = 0;
-	for i = 1:num_layers-1
-		t = Theta{i}(:,2:end);
-		reg = reg + sum(t(:).^2) * lambda;
-	end
 	
-	reg = reg / (2*m);
+	T1 = Theta1(:,2:input_layer_size+1);
+	T2 = Theta2(:,2:hidden_layer1_size+1);
+	T3 = Theta2(:,2:hidden_layer2_size+1);
+	reg = sum(T1(:) .^ 2) * lambda(1) + sum(T2(:) .^ 2)* lambda(2) + sum(T3(:) .^ 2)* lambda(3);
+	reg = (reg * lambda(1)) / (2*m);
 	
 	J = J + reg;
-
+	
 	%computing gradient
+	del4 = a4 - Y;
 	
-	delta = cell(num_layers,1);
-	del = cell(num_layers,1);
+	del3 = (del4 * Theta3) ;
+	del3 = del3(:,2:hidden_layer2_size+1);
+	del3 = del3 .* sigmoidGradient(z3);
 	
-	for i = num_layers:-1:1
-		if(i==num_layers)
-			del{i} = (A{i} - Y);
-		else 
-			if(i == 1)
-				delta{i} = (del{i+1})' * A{i};  
-			else
-				del{i} = del{i+1} * Theta{i};
-				del{i} = del{i}(:,2:end) .* sigmoidGradient(Z{i});
-				delta{i} = (del{i+1})' * A{i};  	
-			end
-		end
-	end
+	del2 = (del3 * Theta2) ;
+	del2 = del2(:,2:hidden_layer1_size+1);
+	del2 = del2 .* sigmoidGradient(z2);
 	
-	Theta_grad = cell(num_layers-1,1);
-	for i = 1:num_layers - 1
-		Theta_grad{i} = delta{i} ./ m;
-	end
+	delta3 = del4' * a3;  
+	delta2 = del3' * a2;  
+	delta1 = del2' * a1;
+	
+	Theta1_grad = delta1 ./ m;
+	Theta2_grad = delta2 ./ m;
+	Theta3_grad = delta3 ./ m;
+	
+	Theta1_grad(:,2:input_layer_size+1) = Theta1_grad(:,2:input_layer_size+1) + Theta1(:,2:input_layer_size+1) .* (lambda(1) / m);
+	Theta2_grad(:,2:hidden_layer1_size+1) = Theta2_grad(:,2:hidden_layer1_size+1) + Theta2(:,2:hidden_layer1_size+1) .* (lambda(2) / m);
+	Theta3_grad(:,2:hidden_layer2_size+1) = Theta3_grad(:,2:hidden_layer2_size+1) + Theta3(:,2:hidden_layer2_size+1) .* (lambda(3) / m);
 
-	grad = [];
-	for i = 1:num_layers - 1
-		Theta_grad{i}(:,2:end) = Theta_grad{i}(:,2:end)  + Theta{i}(:,2:end) .* (lambda/m);
-		
-		% Unroll gradients	
-		grad = [grad ; Theta_grad{i}(:)];
-	end
+	% Unroll gradients
+	grad = [Theta1_grad(:) ; Theta2_grad(:); Theta3_grad(:)];
 	
 end
