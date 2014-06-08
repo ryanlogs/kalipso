4ed4cc2f98201c4d9085101b8fb976d5cdd17cc0
ryanlogs <ryanlogs@gmail.com>
2014-06-08 21:58:15 +0530

    final commit

--- function/nn_functions/main_predict.m
+++ function/nn_functions/main_predict.m
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 function [finalPredict] = main_predict(Main_Theta, X)
-=======
-function p = main_predict(Main_Theta, X)
->>>>>>> ffa283e83d83658e96af5d4f6a0492273b4383b8
 %PREDICT Predict the label of an input given a trained neural network
 %   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
 %   trained weights of a neural network (Theta1, Theta2)
