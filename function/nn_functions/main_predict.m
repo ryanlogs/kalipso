40805b8a1b771df45980ca2e64d36d96ddb60232
ryanlogs <ryanlogs@gmail.com>
2014-06-09 19:44:21 +0530

    Revert "changed main"
    This reverts commit f6b61d1edb64ddc7d305ec3a8844d2fa205f1fed.

--- function/nn_functions/main_predict.m
+++ function/nn_functions/main_predict.m
@@ -8,7 +8,7 @@ m = size(X, 1);
 
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 10);
-
+finalPredict = -1 .* ones(m,1);
 for digit = 0:9
 	H = X;
 	Theta = Main_Theta{digit+1};
@@ -16,17 +16,15 @@ for digit = 0:9
 		H = hyperbolic([ones(m,1) H] * (Theta{i})');
 	end
 	for i = 1:m
-		if(H(i,1) > H(i,2))
-			p(i,digit+1) = -1 * H(i,2);
-		else
-			p(i,digit+1) =  H(i,1);	
+		if(finalPredict(i) == -1 &&  H(i,2) > H(i,1))
+			finalPredict(i) = digit;
 		end	
 	end	
 %	p(:,digit+1) = H(:,2);
 end
 	
-[dummy, finalPredict] = max(p, [], 2);
-finalPredict = finalPredict - 1; 
+%[dummy, finalPredict] = max(p, [], 2);
+%finalPredict = finalPredict - 1; 
 
 % =========================================================================
 
